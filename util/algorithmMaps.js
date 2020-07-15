4debe32625b70cecfeb7b3e6013578522c1dd959
xingzhewang <xingzhewang@sohu-inc.com>
2020-07-15 18:37:12 +0800

    upd

--- util/algorithmMaps.js
+++ util/algorithmMaps.js
@@ -1,6 +1,5 @@
 const jjencode = require('./jjencode');
 const aaencode = require('./aaencode');
-// const obfuscator = require('javascript-obfuscator').obfuscate;
 const obfuscator = require('./obfuscator').obfuscate;
 
 module.exports = {
